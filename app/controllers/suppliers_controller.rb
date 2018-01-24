a03536d88f7025e5215dd55fc089772a04687725
ak <atulkanswal@gmail.com>
2018-01-24 12:08:12 +0530

    [bug fix]
    Signed-off-by: ak <atulkanswal@gmail.com>

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -6,10 +6,10 @@ class SuppliersController < ApplicationController
 
   def show
     @supplier = Supplier.find_by(id: params[:id])
-    if params[:inactive_check].present? && params[:inactive_check] == true
+    if (params[:inactive_check].present? && params[:inactive_check] == "false") || params[:inactive_check].blank?
       @products = @supplier.products.active
-    else
-      @products = @supplier.products
+    elsif params[:inactive_check].present? && params[:inactive_check] == "true"
+      @products = @supplier.products.inactive
     end
   end
 end
