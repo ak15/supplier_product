d306e835e5496d335900a904284e044a6a02dc1e
ak <atulkanswal@gmail.com>
2018-01-24 11:22:13 +0530

    [Fix for sorting]
    Signed-off-by: ak <atulkanswal@gmail.com>

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -1,7 +1,7 @@
 class SuppliersController < ApplicationController
   def index
     @suppliers = Supplier.includes(:products).group("suppliers.id, products.id")
-    .order("(sum(case products.status when 0 then 1 else 0 end)/sum(products.id))*100, suppliers.id desc").references(:products)
+    .order("(sum(case products.status when 0 then 1 else 0 end)/sum(case products.status when 0 then 1 else 1 end))*100 desc").references(:products)
   end
 
   def show
